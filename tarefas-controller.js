e1f9579346a1c5dedf7810a2b5b38431f9a9371b
souamandaleal <amanda.cassiano.leal@gmail.com>
2021-03-29 19:58:09 -0300

    atualizando meteodo put

--- tarefas-controller.js
+++ tarefas-controller.js
@@ -11,13 +11,13 @@
                 return next(err) 
             }
         })
-        res.send('tarefa cadastrada com sucesso.')
+        res.send('tarefa cadastrada com sucesso')
     }
 
     exports.listarTarefas = function (req, res) {
         Tarefas.find({}, function (err, tarefas) {
             if (err) return next(err)
-            return res.json(tarefas);
+            return res.json(tarefas)
         })
     }
 
@@ -28,25 +28,25 @@
         })
     }
 
-    exports.deletarTarefa = function (req, res) {
+   exports.deletarTarefa = function (req, res) {
         Tarefas.deleteOne({_id: req.params.id}, (err) => {
             //Retornar erro quando não conseguir apagar no banco de dados
-            if(err) return res.status(400).json("Error: Tarefa não foi apagada com sucesso!");
+            if(err) return res.status(400).json("Error: Tarefa não foi apagada com sucesso!")
 
             //Retornar mensagem de sucesso quando excluir o registro com sucesso no banco de dados
-            return res.json("Tarefa excluida com sucesso!");
-        });
+            return res.json("Tarefa excluida com sucesso!")
+        })
     }
 
-    exports.alterarTarefa = function (req, res) {
+   exports.alterarTarefa = function (req, res) {
     Tarefas.findById(req.params.id, function(err, tarefa){
             if (err) return next(err)
 
             tarefa.descricao = req.body.descricao;
             terefa.prazo = req.body.prazo;
-            completa = req.body.completa;
+            tarefa.completa = req.body.completa;
 
             return res.json(tarefa)
 
         })
-    }
+}
